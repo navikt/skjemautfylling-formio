78f434f60375d1aa5d46bca808cd4ac7d6c0cc52
Ann Katrin Gagnat <ann.katrin.gagnat@nav.no>
2022-10-12 08:41:44 +0200

    Simple script for notifying 'bygger' on FyllUt deployment status (success or failure)

---
+++ bin/README.md
@@ -0,0 +1,18 @@
+# Hvordan teste notifyDeploy.mjs fra lokal maskin
+
+Opprett en .env fil i denne mappen:
+
+    PUSHER_APP_SECRET=<pusher-app-secret>
+    BYGGER_URL=https://skjemabygging-experimental.dev.nav.no
+    # BYGGER_URL=http://localhost:8080
+
+Pusher secret må tilhøre den pusher-instansen som byggeren også er koblet mot.
+
+Kall skriptet med `testdata`-filen og `success|failure` som input:
+
+    cat testdata | node notifyDeploy.mjs success
+    cat testdata | node notifyDeploy.mjs failure
+
+Endre på innholdet i `testdata` for å teste ulike hendelser.
+
+Vær innlogget i byggeren for å se at meldingen vises som forventet.
---
+++ bin/notifyDeploy.mjs
@@ -0,0 +1,53 @@
+#!/usr/bin/env node
+import fs from "fs";
+import { createHash } from "crypto";
+import axios from "axios";
+import dotenv from "dotenv";
+
+if (process.env.NODE_ENV !== "test") {
+  dotenv.config();
+}
+
+const hash = (password) => createHash("sha256").update(password).digest("hex");
+
+const getEnv = (key) => {
+  const appValue = process.env[key];
+  if (!appValue) {
+    throw new Error(`Must specify env var ${key}`);
+  }
+  return appValue;
+};
+
+const args = process.argv.slice(2);
+const eventType = args[0];
+const usageMessage = "Usage: node bin/notifyDeploy.mjs <success|failure>";
+if (eventType !== "success" && eventType !== "failure") {
+  console.error(usageMessage);
+  process.exit(1);
+}
+const pusherSecret = getEnv("PUSHER_APP_SECRET");
+const byggerUrl = getEnv("BYGGER_URL");
+
+const stdinContent = fs.readFileSync(0, "utf-8");
+const githubEventMessage = JSON.parse(stdinContent);
+
+(async () => {
+  try {
+    await axios.post(
+      `${byggerUrl}/notifications`,
+      {
+        type: eventType,
+        githubEventMessage,
+      },
+      {
+        headers: {
+          "Bygger-Pusher-Secret-Hash": `${hash(pusherSecret)}`,
+        },
+      }
+    );
+    console.log(`Notified bygger: ${eventType}`);
+  } catch (e) {
+    console.error(`Failed to notify bygger: ${e.message}`);
+    process.exit(1);
+  }
+})();
